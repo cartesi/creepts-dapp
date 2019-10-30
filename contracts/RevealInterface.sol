982cc57ba3e6d8c2a0e7f763a872776231f8cf8e
felipeargento <felipe@cartesi.io>
2019-10-30 17:55:33 +0800

    Update RevealInterface

--- contracts/RevealInterface.sol
+++ contracts/RevealInterface.sol
@@ -4,25 +4,42 @@ pragma solidity ^0.5.0;
 
 import "./Instantiator.sol";
 
-
 contract RevealInterface is Instantiator {
 
     enum state {
         CommitPhase,
         RevealPhase,
-        PhaseFinished
+        CommitRevealDone
     }
 
     function instantiate(
         uint256 _commitDuration,
         uint256 _revealDuration,
-        bytes32 _initialHash) public returns (uint256);
+        uint256 _scoreWordPosition,
+        uint256 _logDrivePosition,
+        uint256 _scoreDriveLogSize,
+        uint256 _logDriveLogSize,
+        bytes32 _setupHash) public returns (uint256);
 
     function getScore(uint256 _index, address _playerAddr) public returns (uint256);
+
     function getInitialHash(uint256 _index, address _playerAddr) public returns (bytes32);
+
     function getFinalHash(uint256 _index, address _playerAddr) public returns (bytes32);
-    function hasRevealed(uint256 _index, address _playerAddr) public returns (bool);
+
     function playerExist(uint256 _index, address _playerAddr) public returns (bool);
-    function getCurrentState(uint256 _index, address concernedAddress) public view returns (bytes32);
+
+    function isConcerned(uint256 _index, address _user) public view returns (bool);
+
+    function getCurrentState(uint256 _index) public view returns (bytes32);
+
+    function getState(uint256 _index, address)
+    public view returns (
+        uint256[6] memory _uintValues,  
+        bytes32 pristineHash,
+        bool hasRevealed,
+
+        bytes32 currentState
+    );
 
 }
