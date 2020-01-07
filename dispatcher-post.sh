5315adccf37e3c6ad19d2b92424c1e749b461e2f
Stephen Chen <stephen@cartesi.io>
2020-01-08 00:27:58 +0800

    Make endpoint, log file parameters

--- dispatcher-post.sh
+++ dispatcher-post.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-rm data-0.json data-1.json
+if [ "$#" -ne 2 ] || ! [ -f "$2" ]; then
+  echo "Usage: $0 <dispatcher enpoint> <log.br.cpio path>" >&2
+  exit 1
+fi
 
-log_hash_0=`docker run -v $(pwd):/program cartesi/machine-emulator:0.1.0-rc5 cartesi-machine-hash --page-log2-size=10 --tree-log2-size=20 --input=/program/dapp_data_0/0.json.br.cpio`
-log_hash_1=`docker run -v $(pwd):/program cartesi/machine-emulator:0.1.0-rc5 cartesi-machine-hash --page-log2-size=10 --tree-log2-size=20 --input=/program/dapp_data_1/1.json.br.cpio`
+rm data.json
 
-dispatcher_ip_0=`docker inspect anutodapp_dispatcher_0_1 | grep IPAddress | tail -n 1 | cut -d'"' -f 4`
-dispatcher_ip_1=`docker inspect anutodapp_dispatcher_1_1 | grep IPAddress | tail -n 1 | cut -d'"' -f 4`
+log_hash=`docker run -v $(pwd):/program cartesi/machine-emulator:0.1.0-rc5 cartesi-machine-hash --page-log2-size=10 --tree-log2-size=20 --input=/program/$2`
 
-sed "s/LogHash/0x$log_hash_0/g" data-template.json > data-0.json
-echo "Committing log hash 0: $log_hash_0 to dispatcher 0: $dispatcher_ip_0..."
-curl -X POST -H "Content-Type: application/json" -d "@data-0.json" $dispatcher_ip_0:3001
+sed "s/LogHash/0x$log_hash/g" data-template.json > data.json
+echo "Committing log hash: $log_hash to dispatcher: $1..."
+curl -X POST -H "Content-Type: application/json" -d "@data.json" $1
 echo "\nDone!"
-
-sed "s/LogHash/0x$log_hash_1/g" data-template.json > data-1.json
-echo "Committing log hash 1: $log_hash_1 to dispatcher 1: $dispatcher_ip_1..."
-curl -X POST -H "Content-Type: application/json" -d "@data-1.json" $dispatcher_ip_1:3001
-echo "\nDone"
