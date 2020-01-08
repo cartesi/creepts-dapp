d35b09c10987e00577f15fd105ca4a4ad8a7fb45
Stephen <stephen@cartesi.io>
2020-01-08 16:00:26 +0800

    Auto-remove container after calculate log hash

--- dispatcher-post.sh
+++ dispatcher-post.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 2 ] || ! [ -f "$2" ]; then
   exit 1
 fi
 
-log_hash=`docker run -v $(pwd):/program cartesi/machine-emulator:0.1.0-rc5 cartesi-machine-hash --page-log2-size=10 --tree-log2-size=20 --input=/program/$2`
+log_hash=`docker run --rm -v $(pwd):/program cartesi/machine-emulator:0.1.0-rc5 cartesi-machine-hash --page-log2-size=10 --tree-log2-size=20 --input=/program/$2`
 
 echo "Committing log hash: $log_hash to dispatcher: $1..."
 sed "s/LogHash/0x$log_hash/g" data-template.json | curl -X POST -H "Content-Type: application/json" -d @- $1
