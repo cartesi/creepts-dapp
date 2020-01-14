1bb6bf4f7c32321fc1f6a56ca8936a8322a0080e
Danilo Tuler <danilo@cartesi.io>
2020-01-14 12:08:41 -0500

    Parameters for truffle exec script

--- instantiate_tournaments.js
+++ instantiate_tournaments.js
@@ -1,11 +1,31 @@
+/**
+ * Truffle script to create new tornaments.
+ * 
+ * Usage:
+ * truffle exec <script> --network <network> --level <level> --commit-duration <duration>
+ */
+
 const contract = require("@truffle/contract");
+const program = require("commander");
 
 const MatchManagerInstantiator = contract(require("@cartesi/tournament/build/contracts/MatchManagerInstantiator.json"));
 const RevealInstantiator = contract(require("@cartesi/tournament/build/contracts/RevealInstantiator.json"));
 const Step = contract(require("@cartesi/machine-solidity-step/build/contracts/Step.json"));
 const DApp = contract(require("./build/contracts/DApp.json"));
 
+program
+    .option('-n, --network <network>', 'Specify the network to use, using artifacts specific to that network. Network name must exist in the configuration')
+    .option('-c, --compile', 'Compile contracts before executing the script')
+    .option('-l, --level <level>', 'Level number [0-7]', 0)
+    .option('--commit-duration <duration>', 'Duration in seconds of commit phase', 200)
+    .option('--epoch-duration <duration>', 'Duration in seconds of epoch phase', 200)
+    .option('--match-duration <duration>', 'Duration in seconds of match phase', 90)
+    .option('--round-duration <duration>', 'Duration in seconds of round phase', 45);
+
 module.exports = async (callback) => {
+
+    program.parse(process.argv);
+    console.log(`Creating tournament for level ${program.level} with commit duration of ${program.commitDuration} seconds`);
 
     try {
         const networkId = await web3.eth.net.getId();
@@ -24,15 +44,15 @@ module.exports = async (callback) => {
             console.log(`${contract.contract_name} => ${contract.address}`);
         });
 
-        const commitDuration = 200;
+        const commitDuration = program.commitDuration;
+        const epochDuration = program.epochDuration;
+        const matchDuration = program.matchDuration;
+        const roundDuration = program.roundDuration;
         const scoreDriveLogSize = 3;
         const logDriveLogSize = 20;
-        const epochDuration = 200;
-        const matchDuration = 90;
-        const roundDuration = 45;
         const finalTime = 1e13;
 
-        var setupHashes = [
+        const setupHashes = [
             "0x83a2b88934ac816c45e810b1c4344b214f88ccc7ecc9b5917cf6051b789974dd",
             "0xdb886e23cce224acb75ae42bdebe44bbf07a3eca51a6629043d1e4e6518a42cb",
             "0x425b43d06f55cce660a04bd4b605536f13fb6dab5072f5ce62f3527e02db8718",
@@ -44,26 +64,23 @@ module.exports = async (callback) => {
         ];
 
         const dapp = await DApp.deployed();
-        for (var i in setupHashes) {
-            const hash = setupHashes[i];
-            const transaction = await dapp.instantiate(
-                RevealInstantiator.address,
-                MatchManagerInstantiator.address,
-                commitDuration,
-                scoreDriveLogSize,
-                logDriveLogSize,
-                hash,
-                i, // level
-                epochDuration,
-                matchDuration,
-                roundDuration,
-                finalTime,
-                Step.address,
-                { from: fromAddress }
-            );
-            console.log(`Tournament ${i} created: ${transaction.tx}`);
-        }
-
+        const hash = setupHashes[program.level];
+        const transaction = await dapp.instantiate(
+            RevealInstantiator.address,
+            MatchManagerInstantiator.address,
+            commitDuration,
+            scoreDriveLogSize,
+            logDriveLogSize,
+            hash,
+            program.level,
+            epochDuration,
+            matchDuration,
+            roundDuration,
+            finalTime,
+            Step.address,
+            { from: fromAddress }
+        );
+        console.log(`Tournament created: ${transaction.tx}`);
         callback();
 
     } catch (e) {
