c0031ec8dacde3c4e73120dd3860d049b0a1bb63
Danilo Tuler <danilo@cartesi.io>
2020-01-16 18:48:41 -0500

    Updates setupHashes and use tagged docker image

--- instantiate_tournaments.js
+++ instantiate_tournaments.js
@@ -53,14 +53,14 @@ module.exports = async (callback) => {
         const finalTime = 1e13;
 
         const setupHashes = [
-            "0x42c87a7ef46cd76687023fa4410e197d1a0b340f8022b6717e7bd7b6b8b6150b",
-            "0x67b7effefed2d306b796f7d0c478e75b976294506cf5d73d162f8553de13f0de",
-            "0x267316e8496c4297e65b3c494cc28f5df5f0c722c7d38916988ba4da48046149",
-            "0xdd8362b010474f23ce6e67d4acc8bf0773864e1841b973cb62c1c316e9001d6d",
-            "0x4060f319fd067a0e60a921b9e74e9c157c0f0590198f277c1cdf9cad4ee2b007",
-            "0x2847ec8110bc1001bc34069a9466f8e1fbe668b6d07278aced4a66b153742a12",
-            "0xa9f0efa1e827d24beb86b04944539a77bf40dfca03f22c821c619c1bd712acb1",
-            "0xb21a49e03da0b85fb2a295a3e8f73947113bb91f01596613e90514b56786dc70"
+            "0x865499c9c9b63cbd85152c7e2e2a14a3b195be1d91787cf99a7bf4fb39a5882d",
+            "0xd3c9fd9c6d88a2df501f561f49639e936c6e10df546422448ce169b61bffeda4",
+            "0x2765fbc1a3c324f8f79c5bbf4a7811c1b0d86e898dd3a798d5a313f60d12c49b",
+            "0x073c5f068671cec95c102abccc48257feb8bdeeee8e141de8836dd23a8ff818e",
+            "0xf622c793c73271c4510c1fd2ca7eaad88ab98f826d4ad96a62f4bcfe8983e064",
+            "0xaecc41f597ce71af0781b68bb16ffc78be44b455d6807ad530cf6516ae9480e9",
+            "0x1e9965749efe399ccdc48b7e249c0912d35dba04efdcd6a0268cf5cb329f68e4",
+            "0x4d5e3cacd0baf9a439ea97958fe299c51caf61dedde3d60e517018bab0d302ca"
         ];
 
         const dapp = await DApp.deployed();
